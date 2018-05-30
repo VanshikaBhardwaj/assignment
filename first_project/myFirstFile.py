73be11ec52fe132d45642155ddc4b82b69d0a9d2
Vanshika Bhardwaj <vanshika.bhardwaj.14@gmail.com>
2018-05-30 16:19:19 +0530

    c1

---
+++ first_project/myFirstFile.py
@@ -0,0 +1,28 @@
+print("hello Python world!")    #1
+
+a="Acad"
+b="View"
+print(a+b)                      #2
+
+print("Enter three variables\n")
+x=input()
+y=input()
+z=input()
+print("x =",x,"\ny =",y,"\nz =",z)    #3
+
+print("Let's get started!")     #4
+
+s = "AcadView"
+course = "Python"
+fees = 5000
+print("s = %s"%s)
+print("course = %s" % course)
+print("fees = %d" % fees)         #5
+
+name = "Tony Stark"
+salary = 1000000
+print('%s' %name ,'%d' %salary)  #6
+
+
+
+
